cfdea785d506aeb455860b8f0984186d625ea9c2
ManojHAT <manoj.anumalla@hashagile.com>
2025-10-13 18:20:50 +0530

    Table Triggers

--- get_table_triggers.sql
+++ get_table_triggers.sql
@@ -5,27 +5,33 @@ RETURNS TABLE (
     table_schema text,
     table_name text,
     trigger_name text,
-    trigger_definition text
+    trigger_definition text,
+    md5 text
 )
 AS $$
 BEGIN
     RETURN QUERY
     WITH split_input AS (
-      SELECTs
+      SELECT
         split_part(s, '.', 1) AS schema_name,
         split_part(s, '.', 2) AS table_name
       FROM unnest(p_schema_tables) AS s
     )
     SELECT
-        n.nspname::text,
-        c.relname::text,
-        t.tgname::text,
-        pg_catalog.pg_get_triggerdef(t.oid, true)::text
+        n.nspname::text AS table_schema,
+        c.relname::text AS table_name,
+        t.tgname::text AS trigger_name,
+        pg_catalog.pg_get_triggerdef(t.oid, true)::text AS trigger_definition,
+        md5(
+            n.nspname || '.' ||
+            c.relname || '.' ||
+            t.tgname || '::' ||
+            pg_catalog.pg_get_triggerdef(t.oid, true)
+        ) AS md5
     FROM pg_catalog.pg_trigger AS t
     JOIN pg_catalog.pg_class AS c ON c.oid = t.tgrelid
     JOIN pg_catalog.pg_namespace AS n ON n.oid = c.relnamespace
     JOIN split_input AS si ON si.schema_name = n.nspname AND si.table_name = c.relname
-    -- A trigger's tgisinternal column will be true for system-generated triggers (e.g., for foreign keys)
     WHERE t.tgisinternal IS FALSE;
 END;
 $$ LANGUAGE plpgsql;
