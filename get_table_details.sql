43482084d99825623cb7dd5d21173bfada1603c7
ManojHAT <manoj.anumalla@hashagile.com>
2025-10-13 18:37:18 +0530

    Table Details

--- get_table_details.sql
+++ get_table_details.sql
@@ -10,7 +10,8 @@ RETURNS TABLE (
     is_nullable TEXT,
     column_default TEXT,
     character_maximum_length INT,
-    ordinal_position INT
+    ordinal_position INT,
+    md5 TEXT
 )
 AS $$
 BEGIN
@@ -23,11 +24,20 @@ BEGIN
         c.is_nullable::text,
         c.column_default::text,
         c.character_maximum_length::int,
-        c.ordinal_position::int
+        c.ordinal_position::int,
+        md5(
+            c.table_schema || '.' ||
+            c.table_name || '.' ||
+            c.column_name || ':' ||
+            c.data_type || ':' ||
+            c.is_nullable || ':' ||
+            COALESCE(c.column_default,'') || ':' ||
+            COALESCE(c.character_maximum_length::text,'') || ':' ||
+            c.ordinal_position::text
+        ) AS md5
     FROM information_schema.columns c
     WHERE (c.table_schema || '.' || c.table_name) = ANY(p_table_list)
     ORDER BY c.table_schema, c.table_name, c.ordinal_position;
 END;
 $$ LANGUAGE plpgsql;
-
 --SELECT * FROM get_table_details(ARRAY['public.employees', 'sales.orders']);
